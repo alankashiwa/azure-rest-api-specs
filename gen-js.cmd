c87868e1c98ebe35ca2d74cc188eec76a99369fd
alankashiwa <alankashiwa@gmail.com>
2021-09-07 08:58:34 +0800

    Add autorest command note

---
+++ gen-js.cmd
@@ -0,0 +1,27 @@
+# Creator
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Creator/Readme.md
+
+# Elevation
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/DEM/Readme.md
+
+# Geolocation
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Geolocation/Readme.md
+
+# Render
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Render/Readme.md
+
+# Route
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Route/Readme.md
+
+# Search
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Search/Readme.md
+
+# Timezone
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Timezone/Readme.md
+
+# Traffic
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Traffic/Readme.md
+
+# Weather
+autorest --typescript --track2 --typescript-sdks-folder=../azure-sdk-for-js ./specification/maps/data-plane/Weather/Readme.md
+
